7d4eb570a3627bca8a59afea27e6d34929aeba59
Ga Takeshi（賀 毅） <77286071+gtakeshi@users.noreply.github.com>
2021-07-19 20:01:40 +0900

    Update main.py
    出力の順序も変えてみた

--- main.py
+++ main.py
@@ -6,3 +6,18 @@ for line in dicts.readlines():
     line = line.split()[0]
     f.write(str(text.count(line.split()[0]))+" "+line+"\n")
 
+# # Edit by Takeshi
+# from collections import OrderedDict
+# dicts = open("dict.txt","r")
+# text = str(open("zhou.txt","r").read())
+# f = open("Answer.txt","w")
+# seqdict = OrderedDict()
+# for line in dicts.readlines():
+#     line = line.split()[0]
+#     seqdict[line] = text.count(line.split()[0])
+    
+# seqdict = OrderedDict(sorted(seqdict.items(), key=lambda t: t[1],reverse=True))
+# print(seqdict)
+# for dic in seqdict:
+#     f.write(str(seqdict[dic])+" "+dic+"\n")
+
